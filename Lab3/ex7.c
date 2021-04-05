501889d36d7ab37af7edf4c0221f83f03acdf660
Bazif Rasool <baziflaherwal@gmail.com>
2021-04-05 22:37:04 +0530

    reformat

--- Lab3/ex7.c
+++ Lab3/ex7.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/mman.h>
@@ -9,31 +9,32 @@
 int main()
 {
 
+    int _fdmem;
+    int *map = NULL;
+    const char memDevice[] = "/dev/mem";
 
-int _fdmem;
-int *map = NULL;
-const char memDevice[] = "/dev/mem";
+    _fdmem = open(memDevice, O_RDWR | O_SYNC);
 
-/* open /dev/mem and error checking */
-_fdmem = open( memDevice, O_RDWR | O_SYNC );
+    if (_fdmem < 0)
+    {
+        printf("Failed to open the /dev/mem !\n");
+        return 0;
+    }
+    else
+    {
+        printf("open /dev/mem successfully !\n");
+    }
 
-if (_fdmem < 0){
-printf("Failed to open the /dev/mem !\n");
-return 0;
-}
-else{
-printf("open /dev/mem successfully !\n");
-}
+    map = (int *)(mmap(0, MAPPED_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, _fdmem, DDR_RAM_PHYS));
 
-map= (int *)(mmap(0,MAPPED_SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,_fdmem,DDR_RAM_PHYS));
+    for (int i = 0; i < 100; i++)
+        printf("content: 0x%x\n", *(map + i));
 
-for(int i=0;i<100;i++)
-	printf("content: 0x%x\n",*(map+i));
+    if (munmap(map, MAPPED_SIZE) == -1)
+    {
+        perror("Error un-mmapping the file");
+    }
+    close(_fdmem);
 
-if (munmap(map,MAPPED_SIZE)==-1){
-perror("Error un-mmapping the file");
-}
-close(_fdmem);
-
-return 0;
+    return 0;
 }
